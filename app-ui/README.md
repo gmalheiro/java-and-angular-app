d12c691c20911fdf6bc91792121a25266783a957
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-02-08 06:20:22 -0300

    Moved files to app-ui folder

--- app-ui/README.md
+++ app-ui/README.md
@@ -1,4 +1,4 @@
-# AngularPrimengTodo
+# App-Ui
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
--- app-ui/src/app/app.component.spec.ts
+++ app-ui/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-primeng-todo'`, () => {
+  it(`should have as title 'app-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-primeng-todo');
+    expect(app.title).toEqual('app-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-primeng-todo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app-ui app is running!');
   });
 });
