bbfd14810c25a05a085ece58d500818a6c63e249
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-09 08:28:30 -0300

    Update in app added primeng

--- app_ui/src/app/app.component.ts
+++ app_ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app_ui';
+
+  onSubmit(f: NgForm) {
+    console.log(f.value); 
+    console.log(f.valid); 
+  }
+
+  
+
 }
