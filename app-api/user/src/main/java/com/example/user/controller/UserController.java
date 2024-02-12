ba0ffce5e90b8b3c65cec36814d12f52230547c1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-11 22:05:06 -0300

    Update in UserController

--- app-api/user/src/main/java/com/example/user/controller/UserController.java
+++ app-api/user/src/main/java/com/example/user/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.user.controller;
 
+import com.example.user.model.User;
 import com.example.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,9 @@ public class UserController {
     @Autowired
     private  UserService userService;
 
+    @PostMapping()
+    public User CreateUser(@RequestBody User user){
+        return userService.CreateUser(user);
+    }
 
 }
